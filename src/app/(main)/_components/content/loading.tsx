fe3a67cf88a805340849c8c531d62f513322b859
Dustin Lennon <dustin.lennon@stelth2000inc.com>
2024-06-10 15:16:53 -0400

    match the number of pseudocards

--- src/app/(main)/_components/content/loading.tsx
+++ src/app/(main)/_components/content/loading.tsx
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <div className="flex w-screen grow flex-col overflow-y-auto px-4 sm:w-full sm:p-6">
       <div className="grid grid-cols-4 gap-8">
-        {"abcd".split("").map((_, i) => (
+        {"abcdefghijklmnopqrstuvwxyz".split("").map((_, i) => (
           <SkeletonCard key={i} />
         ))}
       </div>
